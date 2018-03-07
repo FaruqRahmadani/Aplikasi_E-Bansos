632cd79e229f6bb9591b92ab85bcc869e456c41e
Ahmad Syauqi <ahmadsyauqi@Ahmads-MacBook-Pro.local>
2018-03-08 00:09:00 +0800

    update file setting front end

---
+++ resources/views/master_admin/asset.blade.php
@@ -0,0 +1,35 @@
+{{-- =============== --}}
+{{-- css file --}}
+{{-- =============== --}}
+<link href="{{ URL::asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{ URL::asset('assets/css/font-awesome.min.css')}}" rel="stylesheet">
+<link href="{{ URL::asset('assets/css/datepicker3.css')}}" rel="stylesheet">
+<link href="{{ URL::asset('assets/css/styles.css')}}" rel="stylesheet">
+
+<!--Custom Font-->
+<link href="{{ URL::asset('assets/css/monsterat.css')}}" rel="stylesheet">
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+{{-- =============== --}}
+{{-- batas css file --}}
+{{-- =============== --}}
+
+{{-- =============== --}}
+{{-- js file --}}
+{{-- =============== --}}
+
+<script src="{{ URL::asset('assets/js/jquery-1.11.1.min.js')}}"></script>
+<script src="{{ URL::asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{ URL::asset('assets/js/chart.min.js')}}"></script>
+<script src="{{ URL::asset('assets/js/chart-data.js')}}"></script>
+<script src="{{ URL::asset('assets/js/easypiechart.js')}}"></script>
+<script src="{{ URL::asset('assets/js/easypiechart-data.js')}}"></script>
+<script src="{{ URL::asset('assets/js/bootstrap-datepicker.js')}}"></script>
+<script src="{{ URL::asset('assets/js/custom.js')}}"></script>
+
+{{-- =============== --}}
+{{-- js file --}}
+{{-- =============== --}}
