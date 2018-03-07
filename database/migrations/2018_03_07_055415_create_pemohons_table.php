3b53b93627f729416239995bfc455edc19451deb
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-03-07 14:06:16 +0800

    tambah migration tabel pemohon

---
+++ database/migrations/2018_03_07_055415_create_pemohons_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePemohonsTable extends Migration
+{
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::create('pemohons', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('nik');
+      $table->string('nama');
+      $table->string('tempat_lahir');
+      $table->date('tanggal_lahir');
+      $table->string('pekerjaan');
+      $table->string('alamat');
+      $table->string('rt');
+      $table->string('rw');
+      $table->string('kelurahan');
+      $table->string('kecamatan');
+      $table->string('kota');
+      $table->timestamps();
+    });
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+    Schema::dropIfExists('pemohons');
+  }
+}
