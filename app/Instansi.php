b7a643fb958bf97deaad36918ab2be9eeadcd46f
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-03-15 21:57:46 +0800

    penambahan soft delete pada instansi

--- app/Instansi.php
+++ app/Instansi.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Instansi extends Model
 {
+  use SoftDeletes;
+
   public function Kelurahan(){
     return $this->belongsTo('App\Kelurahan');
   }
