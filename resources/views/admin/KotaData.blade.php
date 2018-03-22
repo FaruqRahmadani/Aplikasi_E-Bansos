209707977d90b402590ffbfea6ddaa330beaf9c5
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-03-22 20:17:56 +0800

    tombol tambah data dan perbaikan indent data kota

--- resources/views/admin/KotaData.blade.php
+++ resources/views/admin/KotaData.blade.php
@@ -4,40 +4,46 @@
     <div class="row">
       <div class="col-md-12">
         <div class="panel panel-default">
-        <div class="panel-body">
-          <div class="col-md-12">
-            <table id="table_id" class="table table-striped table-advance table-bordered">
-              <thead>
-                <tr>
-                  <th class="text-center"> Kab/Kota</th>
-                  <th class="text-center"> Provinsi</th>
-                  <th class="text-center"> Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($Kota as $DataKota)
+          <div class="panel-heading no-padding-top">
+            <button-header
+            judul='Tambah Data Provinsi'
+            url={{route('Tambah-Data-Provinsi')}}
+            ></button-header>
+          </div>
+          <div class="panel-body">
+            <div class="col-md-12">
+              <table id="table_id" class="table table-striped table-advance table-bordered">
+                <thead>
                   <tr>
-                    <td> {{$DataKota->nama_kota}}</td>
-                    <td> {{$DataKota->Provinsi->nama_provinsi}}</td>
-                    <td class="text-center">
-                      <button-edit
+                    <th class="text-center"> Kab/Kota</th>
+                    <th class="text-center"> Provinsi</th>
+                    <th class="text-center"> Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($Kota as $DataKota)
+                    <tr>
+                      <td> {{$DataKota->nama_kota}}</td>
+                      <td> {{$DataKota->Provinsi->nama_provinsi}}</td>
+                      <td class="text-center">
+                        <button-edit
                         url = {{ route('Edit-Data-Kota', ['Id' => IDCrypt::Encrypt($DataKota->id)]) }}
-                      ></button-edit>
-                      <button-delete
+                        ></button-edit>
+                        <button-delete
                         url = {{route('Delete-Data-Kota', ['id' => IDCrypt::Encrypt($DataKota->id)])}}
                         jumlah = {{Daerah::CountKecamatan($DataKota->id)}}
-                      ></button-delete>
-                    </td>
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
+                        ></button-delete>
+                      </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+            {{-- batas isian content --}}
           </div>
-          {{-- batas isian content --}}
         </div>
       </div>
     </div>
   </div>
-</div>
 
 @endsection
