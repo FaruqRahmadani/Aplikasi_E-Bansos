5295e76374aa39c12c9b377c7eacf3496d48af87
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-03-22 20:16:53 +0800

    perbaikan indent file provinsi data

--- resources/views/admin/ProvinsiData.blade.php
+++ resources/views/admin/ProvinsiData.blade.php
@@ -7,56 +7,55 @@
     <div class="row">
       <div class="col-md-12">
         <div class="panel panel-default">
-        <div class="panel-heading no-padding-top">
-          <button-header
+          <div class="panel-heading no-padding-top">
+            <button-header
             judul='Tambah Data Provinsi'
             url={{route('Tambah-Data-Provinsi')}}
-          ></button-header>
-        </div>
-        <div class="panel-body">
-          <div class="col-md-12">
-            <table id="table_id" class="table table-striped table-advance table-bordered">
-              <thead>
-                <tr>
-                  <th class="text-center"> Provinsi</th>
-                  <th class="text-center"> Kab/Kota</th>
-                  <th class="text-center"> Kecamatan</th>
-                  <th class="text-center"> Kelurahan</th>
-                  <th class="text-center"> Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($Provinsi as $DataProvinsi)
+            ></button-header>
+          </div>
+          <div class="panel-body">
+            <div class="col-md-12">
+              <table id="table_id" class="table table-striped table-advance table-bordered">
+                <thead>
                   <tr>
-                    <td>{{$DataProvinsi->nama_provinsi}}</td>
-                    <td class="text-center">
-                      {{Daerah::CountKota($DataProvinsi->id)}}
-                    </td>
-                    <td class="text-center">
-                      {{Daerah::CountKecamatan($DataProvinsi->id)}}
-                    </td>
-                    <td class="text-center">
-                      {{Daerah::CountKelurahan($DataProvinsi->id)}}
-                    </td>
-                    <td class="text-center">
-                      <button-edit
+                    <th class="text-center"> Provinsi</th>
+                    <th class="text-center"> Kab/Kota</th>
+                    <th class="text-center"> Kecamatan</th>
+                    <th class="text-center"> Kelurahan</th>
+                    <th class="text-center"> Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($Provinsi as $DataProvinsi)
+                    <tr>
+                      <td>{{$DataProvinsi->nama_provinsi}}</td>
+                      <td class="text-center">
+                        {{Daerah::CountKota($DataProvinsi->id)}}
+                      </td>
+                      <td class="text-center">
+                        {{Daerah::CountKecamatan($DataProvinsi->id)}}
+                      </td>
+                      <td class="text-center">
+                        {{Daerah::CountKelurahan($DataProvinsi->id)}}
+                      </td>
+                      <td class="text-center">
+                        <button-edit
                         url = {{ route('Edit-Data-Provinsi', ['Id' => IDCrypt::Encrypt($DataProvinsi->id)]) }}
-                      ></button-edit>
-                      <button-delete
+                        ></button-edit>
+                        <button-delete
                         url = {{route('Delete-Data-Provinsi', ['id' => IDCrypt::Encrypt($DataProvinsi->id)])}}
                         jumlah = {{Daerah::CountKota($DataProvinsi->id)}}
-                      ></button-delete>
-                    </td>
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
+                        ></button-delete>
+                      </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
           </div>
-          {{-- batas isian content --}}
         </div>
       </div>
     </div>
   </div>
-</div>
 
 @endsection
