be7e796bd2fb33c1394633a21ed0a4506df8334c
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-03-23 23:54:57 +0800

    helper count jumlah proposal

--- app/Helpers/ProposalHelper.php
+++ app/Helpers/ProposalHelper.php
@@ -5,6 +5,40 @@ use Request;
 
 class ProposalHelper
 {
+  public static function CountProposal($Proposal){
+    return $Proposal->count();
+  }
+
+  public static function CountProposalProses($Proposal){
+    $Jumlah = 0;
+    foreach ($Proposal as $DataProposal) {
+      if ($DataProposal->StatusProposal->last()->status == '0') {
+        $Jumlah += 1;
+      }
+    }
+    return $Jumlah;
+  }
+
+  public static function CountProposalDiterima($Proposal){
+    $Jumlah = 0;
+    foreach ($Proposal as $DataProposal) {
+      if ($DataProposal->StatusProposal->last()->status == '1') {
+        $Jumlah += 1;
+      }
+    }
+    return $Jumlah;
+  }
+
+  public static function CountProposalDitolak($Proposal){
+    $Jumlah = 0;
+    foreach ($Proposal as $DataProposal) {
+      if ($DataProposal->StatusProposal->last()->status == '2') {
+        $Jumlah += 1;
+      }
+    }
+    return $Jumlah;
+  }
+
   public static function Status($Data){
     if ($Data->status == '0') {
       $return = '<span class="indicator label-info"></span>Sedang Diproses';
