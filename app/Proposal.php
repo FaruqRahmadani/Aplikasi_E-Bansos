427e39fa7a6e2c30db27a974cb3d129eeae79786
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-03-23 11:22:22 +0800

    relasi tabel proposal

--- app/Proposal.php
+++ app/Proposal.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Proposal extends Model
 {
-    //
+  public function Instansi(){
+    return $this->belongsTo('App\Instansi');
+  }
+
+  public function StatusProposal(){
+    return $this->hasMany('App\StatusProposal');
+  }
 }
