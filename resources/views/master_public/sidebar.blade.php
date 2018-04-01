09f6ffed21e91f9ddeb1f6e9e77f43314f0a81e9
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-04-01 10:20:21 +0800

    hapus layouts lama halaman depan

--- resources/views/master_public/sidebar.blade.php
+++
@@ -1,8 +0,0 @@
-<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
-  <ul class="nav menu">
-    <li class=""><a href="/"><em class="fa fa-home">&nbsp;</em> Beranda</a></li>
-    <li><a href="/persyaratan"><em class="fa fa-copy">&nbsp;</em> Daftar Persyaratan</a></li>
-    <li><a href="/daftar_proposal"><em class="fa fa-list">&nbsp;</em> Daftar Proposal</a></li>
-    <li><a href="/kontak"><em class="fa fa-paper-plane">&nbsp;</em> Kontak</a></li>
-  </ul>
-</div>
